b0741ec4a4c9daa8fcbaac9ab83fe64b08d5825c
zanya945 <tea915656@gmail.com>
2021-08-19 13:44:21 +0800

    owo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+namespace Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder sb = new StringBuilder("Hello",50);
+            sb.ToString();
+        }
+    }
+}
