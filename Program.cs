e6918d79fe9517657cdcf8e24a807b14c95bf1da
zanya945 <tea915656@gmail.com>
2021-08-21 14:01:16 +0800

    西瓜好電

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
+using System.Collections;
 namespace Cs
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>();
+            list.Add("ab");
+            list.Sort();
+  
+
+
 
             StringBuilder sb = new StringBuilder("Hello",50);
             sb.ToString();
+            sb.Append(new String(" world"));
+            sb.AppendFormat(" Hello");//
+            sb.Replace('H','h');//替換前面參數是要替換的字
+            Console.WriteLine(list.Capacity);
+            sb.Insert(0,"owo ");//插入
+            foreach(string i in list){
+            Console.WriteLine(i);
+            }
+            
         }
     }
 }
